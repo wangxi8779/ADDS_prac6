8206c653f9ff4c110e3a44e282b9246f58d54e53
Xi Wang <wangxi8779@hotmail.com>
2023-05-02 21:16:25 +0930

    add big number files

---
+++ BigNumCalc.cpp
@@ -0,0 +1,13 @@
+#include "BigNumCalc.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+BigNumCalc::BigNumCalc() {}
+list<int> BigNumCalc::buildBigNum(string numString){
+  list<int> l = {1};
+  return l;
+}
+list<int> BigNumCalc::add(list<int> num1, list<int> num2){return num1;}
+list<int> BigNumCalc::sub(list<int> num1, list<int> num2){return num1;}
+list<int> BigNumCalc::mul(list<int> num1, list<int> num2){return num1;}
