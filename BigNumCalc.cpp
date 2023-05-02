bab5676adc2240f4cee93ba0ad6acf970f8b842a
Xi Wang <wangxi8779@hotmail.com>
2023-05-02 21:52:00 +0930

    implement big num calc

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -1,11 +1,37 @@
 #include "BigNumCalc.h"
+#include <math.h>
+#include <string>
 using namespace std;
 
 BigNumCalc::BigNumCalc() {}
 list<int> BigNumCalc::buildBigNum(string numString){
-  list<int> l = {1};
-  return l;
+  list<int> num;
+  int length = numString.length();
+  for (int i = 0; i < length; i++) {
+    int digit = numString[i] - '0';
+    num.push_back(digit);
+  }
+  return num;
 }
-list<int> BigNumCalc::add(list<int> num1, list<int> num2){return num1;}
-list<int> BigNumCalc::sub(list<int> num1, list<int> num2){return num1;}
-list<int> BigNumCalc::mul(list<int> num1, list<int> num2){return num1;}
+list<int> BigNumCalc::add(list<int> num1, list<int> num2){
+  int num = toInt(num1) + toInt(num2);
+  return buildBigNum(to_string(num));
+}
+list<int> BigNumCalc::sub(list<int> num1, list<int> num2){
+  int num = toInt(num1) - toInt(num2);
+  return buildBigNum(to_string(num));
+}
+list<int> BigNumCalc::mul(list<int> num1, list<int> num2){
+  int num = toInt(num1) * toInt(num2);
+  return buildBigNum(to_string(num));
+}
+
+int BigNumCalc::toInt(list<int> numList) {
+  int result = 0;
+  int digits = numList.size();
+  for (int i : numList) {
+    result += pow (10, (digits-1)) * i;
+    digits--;
+  }
+  return result;
+}
--- BigNumCalc.h
+++ BigNumCalc.h
@@ -11,6 +11,7 @@ public:
   std::list<int> add(std::list<int> num1, std::list<int> num2);
   std::list<int> sub(std::list<int> num1, std::list<int> num2);
   std::list<int> mul(std::list<int> num1, std::list<int> num2);
+  int toInt(std::list<int>);
 private:  
 };
 
